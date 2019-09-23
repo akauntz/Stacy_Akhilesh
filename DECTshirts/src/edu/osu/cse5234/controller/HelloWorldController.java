b74dd5b687d9f100d4edac6ad129e6b38bb131f7
akauntz <anastasiakauntz@gmail.com>
2019-09-23 19:19:21 -0400

    controller

---
+++ DECTshirts/src/edu/osu/cse5234/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package edu.osu.cse5234.controller;
+
+public class HelloWorldController {
+
+	void printHello () {
+		System.out.print("Hello");
+	}
+	
+	void printHellowNew() {
+		System.out.print("HelloNew");
+	}
+}
